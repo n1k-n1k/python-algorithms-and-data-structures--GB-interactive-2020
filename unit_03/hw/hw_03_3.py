a826ac644ff40886775843ec200fda393534d1f8
Nik Brezginov <n1k-n1k@inbox.ru>
2020-03-30 21:24:35 +0300

    refactor
    Ограничения задачи: попытайтесь решить задания без использования функций max, min, sum, sorted

--- unit_03/hw/hw_03_3.py
+++ unit_03/hw/hw_03_3.py
@@ -8,7 +8,16 @@ source = [i for i in range(0, randint(2, 10))]
 shuffle(source)
 print(f'source list:\t{source}')
 
-min_i = source.index(min(source))
-max_i = source.index(max(source))
+max_i = min_i = 0
+max_n = min_n = source[0]
+
+for i, n in enumerate(source):
+    if n > max_n:
+        max_n = n
+        max_i = i
+    if n < min_n:
+        min_n = n
+        min_i = i
+
 source[max_i], source[min_i] = source[min_i], source[max_i]
 print(f'result list:\t{source}')
