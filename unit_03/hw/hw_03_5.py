711e3e2e1d7c0e0254b2c6a1ee626f9eb26fcc1d
Nik Brezginov <n1k-n1k@inbox.ru>
2020-03-30 21:25:11 +0300

    refactor
    Ограничения задачи: попытайтесь решить задания без использования функций max, min, sum, sorted

--- unit_03/hw/hw_03_5.py
+++ unit_03/hw/hw_03_5.py
@@ -11,5 +11,17 @@ from random import randint
 source = [randint(randint(-10, 0), randint(0, 10)) for _ in range(randint(5, 15))]
 print(f'source list: {source}')
 
-result = sorted([(s, i) for i, s in enumerate(source) if s < 0])[-1]
-print(f'max negative = {result[0]} with index = {result[1]}')
+negatives = [s for s in source if s < 0]
+if not negatives:
+    print('There are no negative numbers in source list')
+
+else:
+    max_n = negatives[0]
+    max_i = source.index(max_n)
+
+    for i, n in enumerate(source):
+        if max_n < n < 0:
+            max_n = n
+            max_i = i
+
+    print(f'max negative = {max_n} with index = {max_i}')
